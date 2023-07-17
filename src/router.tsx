e528f3fa1cec767eada5c5adcf70ffd68e7ab6d1
yuwol <yuwol036@gmail.com>
2023-07-17 14:08:01 +0900

    feat: begin a project

---
+++ src/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Ara</div>,
+  },
+]);
+
+export default router;
