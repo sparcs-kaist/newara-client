9b3322d5993f2b4cf8b882266654d28b4f862d48
yuwol <yuwol036@gmail.com>
2023-07-18 16:05:58 +0900

    feat(i18n): add i18n

---
+++ src/i18n.ts
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import en from "@/locales/en/common.json";
+import ko from "@/locales/ko/common.json";
+
+await i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      ko: { translation: ko },
+    },
+    fallbackLng: "en",
+    debug: import.meta.env.DEV,
+  });
+
+export default i18n;
