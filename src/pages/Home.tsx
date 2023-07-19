d01aa546d2eb48564b77231c7b116edde960a404
yuwol <yuwol036@gmail.com>
2023-07-19 11:38:47 +0900

    feat(home): add slogan

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useTranslation, Trans } from "react-i18next";
+import classNames from "classnames";
+import styles from "./home.module.scss";
 
 export const Home: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <>
-      <main>Home</main>
+      <main>
+        <h1 className={styles["slogan"]}>
+          <Trans
+            i18nKey="slogan.text"
+            values={{ fast: t("slogan.fast"), accurate: t("slogan.accurate") }}
+            components={[
+              <span className={styles["slogan-bold"]} />,
+              <span
+                className={classNames(
+                  styles["slogan-bold"],
+                  styles["slogan-red"]
+                )}
+              />,
+            ]}
+          />
+        </h1>
+      </main>
     </>
   );
 };
