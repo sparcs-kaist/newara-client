ebba3f38a8a43f9dba6889b7e68c2f600ca09f38
yuwol <yuwol036@gmail.com>
2023-08-01 00:53:04 +0900

    fix(i18n): use i18n from useTranslation

--- src/hooks/i18n.ts
+++ src/hooks/i18n.ts
@@ -1,7 +1,9 @@
-import i18n from "@/i18n";
+import { useTranslation } from "react-i18next";
 
 type LocalName = "ko_name" | "en_name";
 
 export const useLocalName = (): LocalName => {
+  const { i18n } = useTranslation();
+
   return (i18n.language + "_name") as LocalName;
 };
